bf34f05d2f135d59c6a92a1727f70b10c2e7788a
varalaxmi <varadevops2121@gmail.com>
2024-11-07 02:02:00 +0530

    ci-cd

---
+++ cicd/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.48.0"
+    }
+  }
+  backend "s3" {
+    bucket = "varadaws78s-remote-state" 
+    key    = "jenkins"
+    region = "us-east-1"
+    dynamodb_table = "varadaws78s-locking"
+  }
+}
+
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ cicd/variables.tf
@@ -0,0 +1,5 @@
+variable "zone_name" {
+  type        = string
+  default     = "lerndevops.online"
+  description = "description"
+}
