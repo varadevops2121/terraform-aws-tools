df188b948fadbbeb18263ccfad03a6df702c5e19
varalaxmi <varadevops2121@gmail.com>
2024-11-08 22:50:26 +0530

    jenkins-file

--- cicd/main.tf
+++ cicd/main.tf
@@ -30,13 +30,13 @@ module "jenkins_agent" {
   }
 }
 
-resource "aws_key_pair" "tools" {
-  key_name   = "tools"
-  # you can paste the public key directly like this
-  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
-  public_key = file("~/.ssh/tools.pub")
-  # ~ means windows home directory
-}
+# resource "aws_key_pair" "tools" {
+#   key_name   = "tools"
+#   # you can paste the public key directly like this
+#   #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
+#   public_key = file("~/.ssh/tools.pub")
+#   # ~ means windows home directory
+# }
 
 # module "nexus" {
 #   source  = "terraform-aws-modules/ec2-instance/aws"
